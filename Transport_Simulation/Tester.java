bd5f46cfb15f1813b0327311f577871dcd9f96a7
TanishT <tanishtyagi@gmail.com>
2020-12-16 14:51:25 -0500

    Starter Code for Transport Simulation Project: 12/16/2020
    Starter Code for Transport Simulation Project: 12/16/2020

---
+++ Transport_Simulation/Tester.java
@@ -0,0 +1,12 @@
+public class Tester
+{
+	public static void main(String[] args)
+	{
+		Auto a = new Auto(10,20,30);
+		Truck t = new Truck(10,20,30);
+		
+		Vehicle v = new Auto(1,2,3);
+		v.move();
+		System.out.println(v);
+	}
+}
