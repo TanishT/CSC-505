f93351c860b6667bdf03cac37b0a4c62c4675d81
TanishT <tanishtyagi@gmail.com>
2020-12-17 20:39:37 -0500

    integrated transport simulator in project 12/17
    integrated transport simulator in project 12/17

---
+++ Transport_Simulation/TransportSim.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class TransportSim {
+
+  private ArrayList<Vehicle> vehicles;
+
+  public TransportSim()
+  {
+    //complete the constructor
+  }
+
+  public void addVehicle(Vehicle v)
+  {
+     //put Vehicle v into the ArrayList, vehicles
+
+  }
+
+  public void loadTrucks(String item)
+  {
+    //load the param item into all trucks in the simulation
+  }
+
+  public void step()
+  {
+    //instruct all vehicles in the simulation to move once
+  }
+
+  public void run(int steps)
+  {
+    //run the simulation 'steps' times
+  }
+
+  public String toString()
+  {
+    //What do you think this should look like?
+    return "TODO";
+  }
+}
