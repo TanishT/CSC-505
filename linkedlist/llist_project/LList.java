da8c617f6841a86c06e3a20ffe9427329381a4ea
TanishT <tanishtyagi@gmail.com>
2021-01-09 15:19:24 -0500

    made Llist class better formatted
    minor code before starting

--- linkedlist/llist_project/LList.java
+++ linkedlist/llist_project/LList.java
@@ -1,20 +1,20 @@
 public class LList<E>{
-  //attributes
-  //head must always refer (point) to the first link in the list.
-  private Link<E> head;
+    //attributes
+    //head must always refer (point) to the first link in the list.
+    private Link<E> head;
 
-  //constructor
-  public LList<E> {
-  }
+    //constructor
+    public LList<E> {
+    }
 
-  //add a new Link that contains newData to the end of this list
-  public void append(E newData) {
-  }
+    //add a new Link that contains newData to the end of this list
+    public void append(E newData) {
+    }
 
-  public void prepend(E newData) {
-  }
+    public void prepend(E newData) {
+    }
 
-  public String toString() {
-    return "";
-  }
+    public String toString() {
+        return "";
+    }
 }
