d69a1e1936bbf52c51b39fa341611b3a9bd88595
TanishT <tanishtyagi@gmail.com>
2021-01-12 15:32:52 -0500

    added member variables size and tail (1/12)
    first part of llist project

--- linkedlist/llist_project/LList.java
+++ linkedlist/llist_project/LList.java
@@ -6,10 +6,14 @@ public class LList<E> {
     //attributes
     //head must always refer (point) to the first link in the list.
     private Link<E> head;
+    private Link<E> tail; //refers to the last Link in this LList
+    private int size;  //stores the number of items currently in this LList (zero for empty list)
 
     //constructor
     public LList () {
         head = null;
+        tail = null;
+        size = 0;
     }
 
     //add a new Link that contains newData to the end of this list
@@ -23,12 +27,14 @@ public class LList<E> {
             }
             temp.setNext(new Link<E>(newData));
         }
+        size++;
     }
 
     //adds a new link to the beginning of the list
     public void prepend(E d) {
         Link<E> temp = head;
         head = new Link<E>(d, temp);
+        size++;
     }
 
     public String toString() {
@@ -36,14 +42,14 @@ public class LList<E> {
         Link<E> curr = head;
         while (true) {
             if (curr.getNext() == null) {
-                linkedList.append("-->null");
+                linkedList.append(" --> null");
                 break;
             }
             String data = "";
             if (curr == head) {
-                data = curr.getData() + "-->" + curr.getNext().getData();
+                data = curr.getData() + " --> " + curr.getNext().getData();
             } else {
-               data = "-->" + curr.getNext().getData();
+               data = " --> " + curr.getNext().getData();
             }
             linkedList.append(data);
             curr = curr.getNext();
