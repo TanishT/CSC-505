c512d9caa0c7421bb8198db3d96b8337c76cf530
TanishT <38384138+TanishT@users.noreply.github.com>
2021-02-16 12:47:18 -0500

    getters and setters

--- trees/TreeProject/BSTNode.java
+++ trees/TreeProject/BSTNode.java
@@ -14,4 +14,28 @@ public class BSTNode<E extends Comparable<E>> {
         left = null;
         right = null;
     }
+    
+    public E getValue(){
+        return value;
+    }
+    
+    public BSTNode getLeft(){
+        return left;
+    }
+    
+    public BSTNode getRight(){
+        return right;
+    }
+    
+    public E setValue(E n){
+        value = n;
+    }
+    
+    public BSTNode setLeft(BSTNode n){
+        left = n;
+    }
+    
+    public BSTNode setRight(BSTNode n){
+        right = n;
+    }
 }
