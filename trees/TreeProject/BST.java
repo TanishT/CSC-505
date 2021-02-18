d65bbbac6303c06a594df83067aefc241e9c3a08
TanishT <tanishtyagi@gmail.com>
2021-02-18 16:19:44 -0500

    added test method

--- trees/TreeProject/BST.java
+++ trees/TreeProject/BST.java
@@ -1,11 +1,25 @@
 public class BST<E> {
+
+    public static void main(String[] args) {
+        BSTNode<String> a = new BSTNode<String>("apple");
+        BSTNode<String> b = new BSTNode<String>("banana");
+        BSTNode<String> c = new BSTNode<String>("cantalope");
+        BSTNode<String> d = new BSTNode<String>("durian");
+        
+        c.setLeft(b);
+        b.setLeft(a);
+        c.setRight(d);
+        
+        System.out.println(method(c));
+    }
+
     public static String method(BSTNode<String> rt){
         if(rt == null){
             return "";
         } else{
             String out = "";
             out += method(rt.getLeft());
-            out += rt.getValue();
+            out += " " + rt.getValue() + " ";
             out += method(rt.getRight());
             return out;
         }
