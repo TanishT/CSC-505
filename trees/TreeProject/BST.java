5d25169528cedd7b29d73973310942d4206af4de
TanishT <tanishtyagi@gmail.com>
2021-02-18 16:17:08 -0500

    added recursive tostring

--- trees/TreeProject/BST.java
+++ trees/TreeProject/BST.java
@@ -1,18 +1,14 @@
 public class BST<E> {
-    BSTNode<E> root;
-    
-    public BST(BSTNode<E> r) {
-        root = r;
-    }
-    
-    public BST(E v) {
-        root.value = v;
-        root.left = null;
-        root.right = null;
-    }
-    
-    public BST(){
-        root = left = right = null;
+    public static String method(BSTNode<String> rt){
+        if(rt == null){
+            return "";
+        } else{
+            String out = "";
+            out += method(rt.getLeft());
+            out += rt.getValue();
+            out += method(rt.getRight());
+            return out;
+        }
     }
         
 }
