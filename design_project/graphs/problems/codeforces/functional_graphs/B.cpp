42033d5266db5abd8b8d39e5d2d712a683e3814c
TanishT <tanishtyagi@gmail.com>
2021-03-13 16:29:16 -0500

    added func graphs

---
+++ design_project/graphs/problems/codeforces/functional_graphs/B.cpp
@@ -0,0 +1,41 @@
+//https://codeforces.com/contest/1020/problem/B
+//this is to ez for any logic lol
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define f first
+#define s second
+#define maxn 1005
+
+int adjList[maxn];
+int visited[maxn] = {0};
+int n;
+
+void dfs(int src) {
+    if (visited[src]) {
+        cout << src + 1 << " ";
+        return;
+    }
+    visited[src] = true;
+    dfs(adjList[src]);
+}
+
+int main(){
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        a--;
+        adjList[i] = a;
+    }
+    memset(visited, false, sizeof(visited));
+    for (int i = 0; i < n; i++) {
+        memset(visited, false, sizeof(visited));
+        dfs(i);
+    }
+    return 0;
+}
