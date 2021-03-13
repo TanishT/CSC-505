6ca73916764f5c4aa6434c6a000b76f0957a9f5e
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-13 16:34:46 -0500

    added formatting and fileio

--- design_project/graphs/problems/cses/teams/teams.cpp
+++ design_project/graphs/problems/cses/teams/teams.cpp
@@ -15,6 +15,15 @@ int n, m;
 vector<int> adj[maxn];
 int ans[maxn];
 vector<int> color(maxn, -1);
+
+void setIO(string name, bool includeout=false) { // name is nonempty for USACO file I/O
+    ios_base::sync_with_stdio(0); cin.tie(0); // see Fast Input & Output
+    // alternatively, cin.tie(0)->sync_with_stdio(0);
+    freopen((name+".in").c_str(), "r", stdin); // see Input & Output
+    if (includeout) {
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
  
 bool isBipartite(int node) {
     queue<int> q;
@@ -41,32 +50,33 @@ bool isBipartite(int node) {
 }
  
 int main() {
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--; b--;
-        adj[a].pb(b);
-        adj[b].pb(a);
-    }
- 
-    bool bipartite = true;
-    for(int i=0; i < n; ++i) {
-        if(color[i]==-1) {
-            if(!isBipartite(i)) {
-                bipartite = false;
-                break;
-            }
-        }
-    }
- 
-    if (!bipartite) {
-        cout << "IMPOSSIBLE" << endl;
-    } else {
-        for (int i = 0; i < n; i++) {
-            cout << ans[i]+1 << " ";
-        }
-        cout << endl;
-    }
-    return 0; 
+	setIO("teams");
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--; b--;
+		adj[a].pb(b);
+		adj[b].pb(a);
+	}
+
+	bool bipartite = true;
+		for(int i=0; i < n; ++i) {
+		if(color[i]==-1) {
+			if(!isBipartite(i)) {
+				bipartite = false;
+				break;
+			}
+		}
+	}
+
+	if (!bipartite) {
+		cout << "IMPOSSIBLE" << endl;
+	} else {
+		for (int i = 0; i < n; i++) {
+			cout << ans[i]+1 << " ";
+		}
+		cout << endl;
+	}
+	return 0; 
 }
